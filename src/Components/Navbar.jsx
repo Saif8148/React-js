2fd664a6f6f493b0c4f330e926000cf0b101d9f4
Saif8148 <msaif942@gmail.com>
2025-03-21 17:25:42 +0530

    Updated react js

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import {  Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-    <nav class="navbar nav1 navbar-expand-lg bg-success ">
+    <nav class="navbar nav1 navbar-expand-lg bg-warning">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
